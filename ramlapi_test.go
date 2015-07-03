4814c7b595023172add8eb4b4ca44d239103e8f5
buddhamagnet <buddhamagnet@gmail.com>
2015-07-03 11:20:19 +0100

    switch to custom router in tests
    now deal with unsorted has access
    
    custom router operational
    
    complete custom router and eliminate mux
    
    rename test

--- ramlapi_test.go
+++ ramlapi_test.go
@@ -1,13 +1,12 @@
 package ramlapi
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"strings"
 	"testing"
-
-	"github.com/gorilla/mux"
 )
 
 var handlers = map[string]http.HandlerFunc{
@@ -19,13 +18,43 @@ var handlers = map[string]http.HandlerFunc{
 	"DeleteMe": DeleteMe,
 }
 
-var router = mux.NewRouter().StrictSlash(true)
+var router RouterMock
+
+type RouterMock struct {
+	routes [][]string
+}
+
+func (router *RouterMock) Consume(data map[string]string) {
+	router.routes = append(router.routes, []string{
+		data["path"],
+		data["verb"],
+		data["handler"],
+	})
+}
+
+func (router *RouterMock) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.String()
+	log.Printf("PATH: %s\n", path)
+	log.Printf("REQUEST METHOD: %s\n", r.Method)
+	for _, endpoint := range router.routes {
+		if router.Registered(path, endpoint) && router.Registered(r.Method, endpoint) {
+			handler := handlers[endpoint[2]]
+			handler(w, r)
+		}
+	}
+}
+
+func (router *RouterMock) Registered(a string, route []string) bool {
+	for _, b := range route {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 
 func routerFunc(data map[string]string) {
-	router.
-		Methods(data["verb"]).
-		Path(data["path"]).
-		Handler(handlers[data["handler"]])
+	router.Consume(data)
 }
 
 func buildAPI() {
@@ -34,6 +63,7 @@ func buildAPI() {
 }
 
 func GetMe(w http.ResponseWriter, r *http.Request) {
+	//log.Fatal("GETME")
 	w.Write([]byte("GetMe"))
 }
 func PostMe(w http.ResponseWriter, r *http.Request) {
@@ -78,6 +108,7 @@ func TestValidRaml(t *testing.T) {
 }
 
 func TestValidRamlGetAssignments(t *testing.T) {
+	router = RouterMock{}
 	// Build the API and assign handlers.
 	buildAPI()
 	// Cycle through the map and dispatch the appropriate
