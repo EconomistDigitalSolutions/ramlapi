e0751193ea46c6c8da8baeb5c69ef8473a35bf6c
buddhamagnet <buddhamagnet@gmail.com>
2015-08-28 12:08:25 +0300

    add ramlgen tests
    complete ramlgan tests
    
    remove debugging statement

---
+++ ramlgen/ramlgen_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/EconomistDigitalSolutions/ramlapi"
+)
+
+var output = "/%s/test_gen_%d"
+
+func TestGenerate(t *testing.T) {
+	api, _ := ramlapi.ProcessRAML("../fixtures/valid.raml")
+	currentOutput := fmt.Sprintf(output, os.TempDir(), int32(time.Now().Unix()))
+	generate(api, currentOutput)
+	_, err := os.Open(currentOutput)
+	if err != nil {
+		t.Fatalf("Expected output file to exist, got %v\n", err)
+	}
+	os.Remove(currentOutput)
+}
