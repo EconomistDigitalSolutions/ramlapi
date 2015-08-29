cc536d68ff52d700077629826d9b21840fff1a79
buddhamagnet <buddhamagnet@gmail.com>
2015-08-29 23:25:14 +0300

    remove debugging statement

--- ramlgen/ramlgen_test.go
+++ ramlgen/ramlgen_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"testing"
 	"time"
@@ -15,7 +14,6 @@ var output = "/%s/test_gen_%d"
 func TestGenerate(t *testing.T) {
 	api, _ := ramlapi.ProcessRAML("../fixtures/valid.raml")
 	currentOutput := fmt.Sprintf(output, os.TempDir(), int32(time.Now().Unix()))
-	log.Fatal(currentOutput)
 	generate(api, currentOutput)
 	_, err := os.Open(currentOutput)
 	if err != nil {
