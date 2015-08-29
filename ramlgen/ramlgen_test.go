4e876f15c87eae3da2f440de995176648704b818
buddhamagnet <buddhamagnet@gmail.com>
2015-08-29 23:21:43 +0300

    complete ramlgan tests

--- ramlgen/ramlgen_test.go
+++ ramlgen/ramlgen_test.go
@@ -1,28 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/EconomistDigitalSolutions/ramlapi"
 )
 
-func TestValidRAML(t *testing.T) {
-	_, err := ramlapi.ProcessRAML("../fixtures/valid.raml")
-	if err != nil {
-		t.Fatalf("Expected ramlgen to parse valid RAML, got %v\n", err)
-	}
-}
+var output = "/%s/test_gen_%d"
 
-func TestInvalidRAML(t *testing.T) {
-	_, err := ramlapi.ProcessRAML("../fixtures/invalid.raml")
-	if err == nil {
-		t.Fatal("Expected ramlgen to abort on in valid RAML\n")
-	}
-}
-
-func TestValidRAMLData(t *testing.T) {
+func TestGenerate(t *testing.T) {
 	api, _ := ramlapi.ProcessRAML("../fixtures/valid.raml")
-	if api.Title != "gref" {
-		t.Fatalf("Expected API title to be parsed as gref, got %s\n", api.Title)
+	currentOutput := fmt.Sprintf(output, os.TempDir(), int32(time.Now().Unix()))
+	log.Fatal(currentOutput)
+	generate(api, currentOutput)
+	_, err := os.Open(currentOutput)
+	if err != nil {
+		t.Fatalf("Expected output file to exist, got %v\n", err)
 	}
+	os.Remove(currentOutput)
 }
